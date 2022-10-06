d47dfc81eb330bade87b0beeb904dbfe72b7760e
Emmanuel <ejaramilo@hotmail.com>
2022-10-06 09:30:09 -0500

    125 what to add and not to add as dependencies in use effect

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={authCtx.onLogout}></Button>
+      <Button onClick={authCtx.onLogout}>Logout</Button>
     </Card>
   );
 };
